a82eb801d0857412ad9a0866cf06fda1297f36fa
Daoist24 <faridzfaisal60@gmail.com>
2025-11-26 20:49:23 +0800

    Update Package.json

--- database/init/00_setup_auth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copy custom pg_hba.conf to data directory
-cp /docker-entrypoint-initdb.d/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
-# Reload PostgreSQL configuration
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT pg_reload_conf();"
-# Explicitly set the password
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
